5a513d8b3e00d59c6af0d7080e8e100523b8717a
dprotopopov <protopopov@narod.ru>
2014-08-15 22:21:14 +0400

    first commit

---
+++ resavewithbom.bat
@@ -0,0 +1,5 @@
+resavewithbom.exe *.css
+resavewithbom.exe *.html
+resavewithbom.exe *.dxview
+resavewithbom.exe *.js
+pause
---
+++ resavewithbom/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace resavewithbom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string dir = ".";
+            string mask = "*.txt";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (string.CompareOrdinal(args[i], "-d") == 0)
+                {
+                    dir = args[++i];
+                }
+                else
+                {
+                    mask = args[i];
+                }
+            }
+            foreach (FileInfo f in new DirectoryInfo(dir).GetFiles(mask, SearchOption.AllDirectories))
+            {
+                Console.WriteLine(f.FullName);
+                string s = File.ReadAllText(f.FullName, Encoding.Default);
+                File.WriteAllText(f.FullName, s, Encoding.UTF8);
+            }
+        }
+    }
+}
